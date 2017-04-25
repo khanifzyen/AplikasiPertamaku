6bd2d951faeeff86f62a7e76416f86000bc91745
khanifzyen <khanif.zyen@gmail.com>
2017-04-26 06:33:11 +0700

    posting #2

--- app/src/main/java/id/my/khanifzyen/aplikasipertamaku/MainActivity.java
+++ app/src/main/java/id/my/khanifzyen/aplikasipertamaku/MainActivity.java
@@ -2,12 +2,52 @@ package id.my.khanifzyen.aplikasipertamaku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    String msg= "Android : ";
 
+    /** dipanggil ketika activity pertama kali dibuat. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
     }
+
+    /** dipanggil ketika activity akan tampil. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+
+    /** dipanggil ketika activity sudah tampil. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+
+    /** dipanggil ketika activity sedang berfokus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+
+    /** dipanggil ketika activity tidak lagi tampil. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+
+    /** dipanggil ketika sesaat sebelum activity dihancurkan/dinonaktifkan. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
+    }
+
 }
